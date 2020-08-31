d1a22647d902a5dfc7f9dae149e9bffdec34d2d1
Sabil <sabilmdtv1@gmail.com>
2020-08-31 19:44:36 +0100

    React class based component and state

---
+++ seasons/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));

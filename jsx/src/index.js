73b7ba5ff4f9e57c6c87fdac5cd1707a4b8501f1
Sabil <sabilmdtv1@gmail.com>
2020-08-31 17:09:38 +0100

    Initial commit.

---
+++ jsx/src/index.js
@@ -0,0 +1,25 @@
+// Import React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a react component
+const App = () => {
+  const buttonText = "Click me";
+  var d = new Date();
+
+  return (
+    <div>
+      <label className='label' htmlFor='name'>
+        Enter Name:
+      </label>
+      <input id='name' type='text' />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
+      {d.toLocaleTimeString()}
+    </div>
+  );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));

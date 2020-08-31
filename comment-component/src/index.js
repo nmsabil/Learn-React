9584b172f7dcffd7a2df2185684ab5b312018d97
Sabil <sabilmdtv1@gmail.com>
2020-08-31 18:09:54 +0100

    using component inside another using props

--- comment-component/src/index.js
+++ comment-component/src/index.js
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import Card from "./Card";
 
 const App = () => {
   let date = new Date().toDateString();
-  console.log(date);
   return (
     <div className='ui container comments'>
-      <CommentDetail author='Sam' date={date} post='Very nice post' />
-      <CommentDetail author='Alex' date={date} post='damn good' />
-      <CommentDetail author='Jane' date={date} post='Love it' />
+      <Card
+        comment={
+          <CommentDetail author='Sam' date={date} post='Very nice post' />
+        }
+      />
+
+      <Card
+        comment={<CommentDetail author='Alex' date={date} post='damn good' />}
+      />
+
+      <Card
+        comment={<CommentDetail author='Jane' date={date} post='Love it' />}
+      />
+      <Card comment='Are you Sure?' />
     </div>
   );
 };

09eb28d65e57dd71030d7bb06eef79b075e30d26
Sabil <sabilmdtv1@gmail.com>
2020-09-02 19:01:45 +0100

    obtain results from youtube API and display video list

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,15 +1,24 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtubeapi from "../apis/youtubeapi";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    console.log(term);
+  state = { videos: [] };
+  onTermSubmit = async (term) => {
+    const response = await youtubeapi.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
   };
   render() {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }

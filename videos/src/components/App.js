63cbf35326803cbfa4dd5aee57dd8cd4990ea5fe
Sabil <sabilmdtv1@gmail.com>
2020-09-02 19:22:35 +0100

    update state property in parent component from child component using a callback function

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,7 +4,7 @@ import youtubeapi from "../apis/youtubeapi";
 import VideoList from "./VideoList";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
   onTermSubmit = async (term) => {
     const response = await youtubeapi.get("/search", {
       params: {
@@ -14,11 +14,18 @@ class App extends React.Component {
 
     this.setState({ videos: response.data.items });
   };
+
+  onVideoSelect = (video) => {
+    console.log(video);
+  };
   render() {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,13 +1,17 @@
 import React from "react";
+import "./VideoItem.css";
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div>
+    <div onClick={() => onVideoSelect(video)} className='item video-item'>
       <img
+        className='ui image'
         src={video.snippet.thumbnails.medium.url}
         alt={video.snippet.title}
       />
-      {video.snippet.title}
+      <div className='content'>
+        <div className='header'>{video.snippet.title}</div>
+      </div>
     </div>
   );
 };

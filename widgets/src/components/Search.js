37a1c02e65c1088936bddf0e7a0c7884632d9e85
Sabil <sabilmdtv1@gmail.com>
2020-09-04 20:29:05 +0100

    use off axios to fetch wikipedia data. settimeout and return in use effect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,15 +1,56 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Search() {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  console.log(results);
 
   useEffect(() => {
-    console.log("hello");
-  }, []);
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
 
-  useEffect(() => {
-    console.log("hello");
+    const timeOutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500);
+
+    return () => {
+      clearTimeout(timeOutId);
+    };
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className='item' key={result.pageid}>
+        <div className='right floated content'>
+          <a
+            target='_blank'
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className='ui button'
+          >
+            Read More
+          </a>
+        </div>
+        <div className='content'>
+          <div className='header'>{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -24,6 +65,7 @@ function Search() {
           />
         </div>
       </div>
+      <div className='ui celled list'>{renderedResults}</div>
     </div>
   );
 }

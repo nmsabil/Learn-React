5f60c082b1d0e276949c21c8e693f9aa342d43d0
Sabil <sabilmdtv1@gmail.com>
2020-09-08 14:44:27 +0100

    if the option is selected show null

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
@@ -33,15 +33,18 @@ const options = [
   },
 ];
 
-function App() {
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
       <br />
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown options={options} />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
-}
-
-export default App;
+};

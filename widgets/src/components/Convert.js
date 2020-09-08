9ced87725762b4eb2c25add0ba44e81e0701ea50
Sabil <sabilmdtv1@gmail.com>
2020-09-08 17:25:27 +0100

    reducing number of requests to google api with debounce stratagy using settimeout

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -2,20 +2,41 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Convert({ language, text }) {
+  const [translated, setTranslated] = useState("");
+  const [debouncedText, setdebouncedText] = useState(text);
+
   useEffect(() => {
-    axios.post(
-      "https://translation.googleapis.com/language/translate/v2",
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
-        },
-      }
-    );
-  }, [language, text]);
-  return <div></div>;
+    const timerid = setTimeout(() => {
+      setdebouncedText(text);
+    }, 500);
+    return () => {
+      clearTimeout(timerid);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
+  }, [language, debouncedText]);
+  return (
+    <div>
+      <h1 className='ui header'>{translated}</h1>
+    </div>
+  );
 }
 
 export default Convert;

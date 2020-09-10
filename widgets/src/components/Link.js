c162db13e29d3aa9e496741206065dec91a5f08e
Sabil <sabilmdtv1@gmail.com>
2020-09-10 16:46:56 +0100

    navigation without page refresh

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -3,6 +3,10 @@ import React from "react";
 function Link({ className, href, children }) {
   const onClick = (e) => {
     e.preventDefault();
+    window.history.pushState({}, "", href);
+
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
   };
   return (
     <a onClick={onClick} className={className} href={href}>
